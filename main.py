97018c166f63aeb340996550df3a018dbebc7738
Hadj Sassi Mahdi <74431887+HadjSassi@users.noreply.github.com>
2022-09-12 19:50:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,58 @@
-import streamlit as st
+import mysql.connector
 import pandas as pd
 import plotly.express as px
+import streamlit as st
 
-#read from database
-workbook = pd.read_excel('a.xlsx')
+#Connect with database
+#Please verify the informations below to avoid exceptions
+cnx = mysql.connector.connect(host='localhost',user='root',password='system',port='3306',database='cryptos',auth_plugin='mysql_native_password')
 
-#Convert time column to a date column
-workbook["time"] = pd.to_datetime(workbook['time'])
+#fetch all balences in the table get_balence
+cursor = cnx.cursor()
+query = "select * from get_balence;"
+cursor.execute(query)
+myresult = cursor.fetchall()
 
-#prepare the columns
-time = workbook['time']
-value = workbook['coeff mult.5']
-value1 = workbook['coeff mult']
-value2 = workbook['coeff mult.1']
-value3 = workbook['coeff mult.2']
-value4 = workbook['coeff mult.3']
-value5 = workbook['coeff mult.4']
+#create a dataframe from the table get_balence
+column_names = ['id_get_balence','dates','crypto_name','crypto_wallet','id_bot']
+workbook = pd.DataFrame(columns=column_names)
+for i in myresult:
+    workbook.loc[len(workbook.index)] = i
 
-#create the first dataframe
-df1 = pd.concat([time,value], axis=1)
-df1.rename(columns={'coeff mult.5':'Bot Max'},inplace=True)
+#correct fields to the true type property
+workbook["dates"] = pd.to_datetime(workbook['dates'])
+workbook["crypto_wallet"] = pd.to_numeric(workbook['crypto_wallet'])
 
-#prepare the plot of the first dataframe
-fig = px.line(df1, x="time", y=df1.columns,
-              title='custom tick labels')
+#delete useless columns
+workbook = workbook.drop(["id_get_balence","id_bot"], axis=1)
 
-#create the second  dataframe
-df2 = pd.concat([time,value1,value2,value3,value4,value5],axis=1)
-df2.rename(columns={'coeff mult':'ETH','coeff mult.1':'DOT','coeff mult.2':'DODGE','coeff mult.3':'ADA','coeff mult.4':'BNB'},inplace=True)
+#normalize crypto_name field to delete the redundent "/USDT"
+for i in range(workbook.shape[0]):
+    workbook.iloc[i,1] = workbook.iloc[i,1].replace('/USDT','')
 
-#prepare the plot of the second dataframe
-fig2 = px.line(df2, x="time", y=df2.columns,
-              title='custom tick labels')
+#Create a dictionnary that differs between crypto_names
+list_df = {}
+for i in workbook['crypto_name'].unique() :
+    l = ['dates',i]
+    d = pd.DataFrame(columns=l)
+    list_df[i] = d
 
-#showing the two plots
-st.plotly_chart(fig)
+#Fill the dictionnary with the appropriate values
+for i in workbook.values:
+    for j,k in list_df.items():
+        if (i[1] == j):
+            k.loc[len(k.index)] = [i[0],i[2]]
+
+#Make the final dataframe that contains all the necessary values
+dff = pd.concat(list(list_df.values()))
+dff = dff.sort_values('dates',ignore_index=True)
+
+#fill the nan values with a logic one
+dff = dff.fillna(method='ffill')
+dff = dff.fillna(0)
+
+#create a plotly chart
+fig2 = px.line(dff, x="dates", y=dff.columns, title='cryptos showed by date and wallet')
+
+#show the chart with streamlit
 st.plotly_chart(fig2)
